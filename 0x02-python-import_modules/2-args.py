e96efcbbc03ddb4bbc894f5be146c2ecebf7217f
KuntaWasonga <wkunta@rocketmail.com>
2022-08-25 15:46:40 -0700

    Ensured code is unexecutable when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
-import sys
 
-n = len(sys.argv)
+if __name__ == "__main__":
+    """Program that prints number of and the list of arguments"""
+    import sys
 
-if n == 1:
-    print("0 arguments.")
-else:
-    if n == 2:
-        print("1 argument:")
+    n = len(sys.argv)
+
+    if n == 1:
+        print("0 arguments.")
     else:
-        print("{} arguments:".format(n-1))
-    for i in range(1, n):
-        print("{}: {}".format(i, sys.argv[i]))
+        if n == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments:".format(n-1))
+        for i in range(1, n):
+            print("{}: {}".format(i, sys.argv[i]))

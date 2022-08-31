474a61fa757f933a9b8c474601b71d9c3ba7fdd9
KuntaWasonga <wkunta@rocketmail.com>
2022-08-31 14:26:39 -0700

    Corrected code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    row = len(matrix)
-    column = len(matrix[0])
-    new = [[0 for _ in range(column)] for _ in range(row)]
-
-    # iterate through columns of matrix
-    for i in range(column):
-        # iterate through rows of matrix
-        for j in range(row):
-            new[i][j] += matrix[i][j] * matrix[i][j]
-
-    return new
+    return ([list(map(lambda x: x * x, row)) for row in matrix])

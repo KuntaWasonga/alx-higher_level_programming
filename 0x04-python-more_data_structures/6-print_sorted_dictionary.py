03d948bcf80817a5f680599c47445bc1526ca8e1
KuntaWasonga <wkunta@rocketmail.com>
2022-08-31 12:08:19 -0700

    Pycodestyle correction

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,4 +3,4 @@ def print_sorted_dictionary(a_dictionary):
     new = dict(sorted(a_dictionary.items()))
 
     for k, v in new.items():
-        print("{}: {}".format(k,v))
+        print("{}: {}".format(k, v))

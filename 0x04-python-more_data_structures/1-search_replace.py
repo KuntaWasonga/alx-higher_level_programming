2a2da10df70484c84c1e9a30d2af61f63c29620a
KuntaWasonga <wkunta@rocketmail.com>
2022-08-31 12:21:52 -0700

    Corrected pycodestyle

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,6 @@
 def search_replace(my_list, search, replace):
     new = my_list[:]
     for i in range(len(new)):
-            if new[i] == search : new[i] = replace
+        if new[i] == search:
+            new[i] = replace
     return new

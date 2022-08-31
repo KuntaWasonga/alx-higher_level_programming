fcd87aa71a41185296087be30918bbe4a4aa85f0
KuntaWasonga <wkunta@rocketmail.com>
2022-08-31 12:02:02 -0700

    Corrected code

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    for i in range(len(my_list):
-            if my_list[i] == search : my_list[i] = replace
+    new = my_list[:]
+    for i in range(len(new)):
+            if new[i] == search : new[i] = replace
+    return new

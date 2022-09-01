85d73fdb16be8a9c052f70e3ce28c2eb768126eb
KuntaWasonga <wkunta@rocketmail.com>
2022-09-01 02:20:36 +0000

    Corrected pycodestyle

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
             roman_string is None):
         return (0)
@@ -20,7 +21,7 @@ def roman_to_int(roman_string):
             return (0)
 
         if (i != (len(roman_string) - 1) and
-           roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]])
+                roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
                 num += roman_dict[roman_string[i]] * -1
 
         else:

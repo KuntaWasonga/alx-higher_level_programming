a9c8843774406aeb0550b48fe16eeb3281f3618a
KuntaWasonga <wkunta@rocketmail.com>
2022-09-14 15:09:54 -0700

    Added documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 class Square:
-    """Class Square that defines a square object
-    """
+    """Class Square that defines a square object."""
+
     def __init__(self, size):
         """Initialize method that stores the size of the square
         Args:

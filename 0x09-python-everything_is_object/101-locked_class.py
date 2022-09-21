ea2d00d0a62ea422b4b9a126ef8d3753a399a80d
KuntaWasonga <wkunta@rocketmail.com>
2022-09-21 03:36:33 -0700

    Advnaced task 1

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -3,6 +3,8 @@
 
 
 class LockedClass:
+    """Prevents user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'."""
     __slots__ = ['first_name']
 
     def __init__(self):

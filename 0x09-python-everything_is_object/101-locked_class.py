ebfa5967f9954b7e5e47d274eac1bdc7f0405a12
KuntaWasonga <wkunta@rocketmail.com>
2022-09-20 19:50:38 -0700

    Changed permissions

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-class LockedClass:
+"""Contains a class that avoids dynamically created attributes."""
 
+
+class LockedClass:
     __slots__ = ['first_name']
 
-    def __init__(self, first_name=''):
-        self.first_name = first_name
+    def __init__(self):
+        """Initialisation method"""
+        pass

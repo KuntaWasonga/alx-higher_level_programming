e84c1116f53362bfbd3d84b8b4688037725db674
KuntaWasonga <wkunta@rocketmail.com>
2022-09-28 13:59:15 -0700

    Corrected code

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,13 +3,8 @@
 
 
 def read_file(filename=""):
-        """ Function that reads from a file
-            Args:
-                        filename: filename
-                            Raises
-                                    Exception: when the file can be opened
-                                        """
-
-                                            with open(filename, 'r', encoding="utf-8") as f:
-                                                    read_data = f.read()
-                                                            print(read_data, end='')
+    """ Function that reads from a file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data, end='')

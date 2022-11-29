271519edc8ffdf41a87fcbb3c0f23b62a5130c00
KuntaWasonga <wkunta@rocketmail.com>
2022-11-29 00:55:36 +0000

    Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+let n = parseInt(process.argv[2]);
+if (Number.isNaN(n)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0, s; i < n; i++) {
+    s = '';
+    for (let j = 0; j < n; j++) {
+      s += 'X';
+    }
+    console.log(s);
+  }
+}

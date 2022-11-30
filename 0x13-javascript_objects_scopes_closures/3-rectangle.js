73f9c7d994d01c6262de1ce1d8022fb545068848
KuntaWasonga <wkunta@rocketmail.com>
2022-11-30 13:21:29 +0300

    Task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+class Rectangle {
+    constructor(w,h) {
+        if ((w > 0) || (h > 0)) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+
+    print() {
+        for (let i = 0; i < h; i++){
+            for (j = 0; j < w; j++){
+                s += 'X';
+            }
+            s += '\n';
+        }
+    }
+
+    console.log(s);
+}
+
+module.exports = Rectangle;
